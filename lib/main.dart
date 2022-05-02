0fa090a35c784108eade21a0a9cd17fb369aa3db
user <user@Abduraufs-MacBook-Pro.local>
2022-05-03 03:08:32 +0500

    delete category added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:kfcapp/provider/remove_category_provider.dart';
 import 'package:kfcapp/provider/set_state_provider.dart';
 import 'package:kfcapp/provider/sign_up_page_provider.dart';
 import 'package:kfcapp/routes/my_routes.dart';
 import 'package:provider/provider.dart';
-void main() async{
-  
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: ((context) => SetStateProvider())),
-        ChangeNotifierProvider(create: ((context) => SignUpPageProvider())),
-      ],
-      child: MyApp()),);
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(create: ((context) => SetStateProvider())),
+      ChangeNotifierProvider(create: ((context) => SignUpPageProvider())),
+      ChangeNotifierProvider(create: ((context) => RemoveCategoryProvider())),
+    ], child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/admin/update/delete_category_page.dart
+++ lib/screens/admin/update/delete_category_page.dart
@@ -1,12 +1,72 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:kfcapp/core/constants/font_const.dart';
+import 'package:kfcapp/core/constants/pm_const.dart';
+import 'package:kfcapp/core/constants/radius_const.dart';
+import 'package:kfcapp/provider/remove_category_provider.dart';
+import 'package:kfcapp/service/firestorage_service.dart';
+import 'package:provider/provider.dart';
 
 class DeleteCategoryPage extends StatelessWidget {
-  const DeleteCategoryPage({ Key? key }) : super(key: key);
+  const DeleteCategoryPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: ListView.builder(
+        itemCount: UserAddImage.foods.length,
+        itemBuilder: ((context, index) {
+          return listViewBuilderItem(index, context);
+        }),
+      ),
+    );
+  }
+
+  Widget listViewBuilderItem(int index, BuildContext context) {
+    return Container(
+      margin: PMConst.kMediumPM,
+      height: MediaQuery.of(context).size.height * 0.15,
+      width: MediaQuery.of(context).size.width * 0.8,
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.05),
+          borderRadius: BorderRadius.circular(RadiusConst.kLargeRadius)),
+      child: Row(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.1,
+            width: MediaQuery.of(context).size.height * 0.1,
+            margin: PMConst.kMediumPM,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(RadiusConst.kMediumRadius),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: CachedNetworkImageProvider(
+                  UserAddImage.foods[index]["category_photo"],
+                ),
+              ),
+            ),
+          ),
+          Text(
+            UserAddImage.foods[index]["category_name"],
+            style: const TextStyle(fontSize: FontConst.kMediumFont + 3),
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: () async {
+                    await context
+                        .read<RemoveCategoryProvider>()
+                        .removeCategoryProvider(index);
+                  },
+                  icon: const Icon(Icons.delete_outline),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
